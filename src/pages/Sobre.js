d1044eb918738066414e137140821b2ae2394d70
felipehads <felipehads@outlook.com>
2020-11-19 13:14:23 -0200

    Consertando erro da página Sobre

--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -6,15 +6,9 @@ import { color } from 'react-native-reanimated';
 export default ({navigation}) => {
     return(
         <ScrollView style={styles.container}>
-<<<<<<< HEAD
             <View style={styles.headerContainer}>
                 <Text style={styles.headerText}>
                     SOBRE O APLICATIVO
-=======
-            <View>
-                <Text style ={styles.header}>
-                    SOBRE O{"\n"} APLICATIVO
->>>>>>> d6035882c2eb35dbb00fac713a67c7bf5ad5263c
                 </Text>
             </View>
             <View style={styles.textContainer}>
@@ -22,7 +16,6 @@ export default ({navigation}) => {
                     Apresentação O câncer de mama é uma doença que vem apresentando alta incidência em todo o mundo. Para o triênio 2020-2022 estima-se mais de 66 mil novos casos da doença no Brasil. Diante disso, o trabalho dos profissionais de saúde é fundamental para redução das taxas de incidência e mortalidade dessa doença. Assim, os profissionais precisam estar aptos para prestarem assistência de qualidade. Neste contexto o aplicativo MAMAprev foi desenvolvido com o intuito de auxiliar os profissionais da saúde que atuam no cuidado das mulheres, por meio de uma ferramenta informativa segura e baseada em evidências científicas, no fortalecimento das ações de prevenção do câncer de mama oportunamente, ou seja, aproveitando a presença da mulher no serviço de saúde independente do motivo, para informá-la sobre a doença e procedimentos de triagem, fatores de risco, rastreamento, favorecendo o diálogo a respeito da realização dos exames para detecção precoce e rastreamento do câncer de mama. Sobre Nós Este aplicativo é produto de uma dissertação de mestrado do Programa de Pós-Graduação em Saúde e Tecnologia (PPGST) da Universidade Federal do Maranhão (UFMA) campus avançado Imperatriz, tendo como pesquisadoras responsáveis a professora doutora Adriana Gomes Nogueira Ferreira e a mestranda Rafaela Cristina Araújo Gomes. A pesquisa faz parte de um projeto financiado pela Fundação de Amparo à Pesquisa e ao Desenvolvimento Científico e Tecnológico do Maranhão (FAPEMA), intitulado “PREVENÇÃO SECUNDÁRIA DO CÂNCER DE MAMA: USO DE APLICATIVO MÓVEL PARA CAPACITAÇÃO DE ENFERMEIROS”. Referências AMERICAN COLLEGE OF RADIOLOGY (ACR). Atlas BI-RADS do ACR: sistema de laudos e registro de dados de imagem da mama - 2 ed. São Paulo: Colégio Brasileiro de Radiologia, 2016. ISBN 978-85-786-8249-1. NATIONAL CANCER INSTITUTE. Breast Cancer Risk Assessment Tool. Disponível em : https://bcrisktool.cancer.gov/calculator.html INSTITUTO NACIONAL DO CÂNCER JOSÉ ALENCAR GOMES DA SILVA (INCA). Diretrizes para a detecção precoce do câncer de mama no Brasil – Rio de Janeiro, 2015. ISBN 978-85-7318-274-3. SOCIEDADE BRASILEIRA DE MASTOLOGIA (SBM). Câncer de Mama: Consenso da Sociedade Brasileira de Mastologia – Regional Piauí. 2017.
                 </Text>
             </View>
-            
         </ScrollView>
     )
 }
@@ -32,7 +25,6 @@ const styles = StyleSheet.create({
         flex:1,
         backgroundColor: "#ffffff"
     },
-<<<<<<< HEAD
     headerContainer:{
         top: 66,
         width:280,
@@ -56,25 +48,5 @@ const styles = StyleSheet.create({
         opacity: 0.7,
         width: 298,
         lineHeight: 41
-=======
-    header:{
-        fontSize:34,
-        marginTop: 50,
-        marginRight: 20,
-        fontWeight: "bold",
-        textAlign: "right"
-    },
-    bigText:{
-        fontSize:25,
-        fontWeight: "bold",
-        marginVertical: 10,
-        marginLeft: 20,
-    },
-    text:{
-        fontSize:18,
-        marginHorizontal:30,
-        textAlign: "justify",
-        fontWeight: "600",
->>>>>>> d6035882c2eb35dbb00fac713a67c7bf5ad5263c
     }
 })

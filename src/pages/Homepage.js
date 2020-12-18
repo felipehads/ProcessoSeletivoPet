60ffe183cfdcc3395ec658ee6ed67fbc61e37481
felipehads <felipehads@outlook.com>
2020-12-17 21:01:04 -0300

    Modal

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import {View,Text, StyleSheet, StatusBar} from 'react-native';
+import React, { useState } from 'react';
+import {View,Text, StyleSheet, StatusBar, Modal} from 'react-native';
 import NewButton from '../components/NewButton';
 
+
+
 const Homepage = ({navigation}) => {
+    const[visible, setVisible] = useState(true);
+    const[opacity, setOpacity] = useState(0.6);
+
     return(
         <View style={styles.container}> 
             <StatusBar hidden={true} />
-            <View style={styles.button}>
-                <Text style={styles.text}>Muito bem! Agora selecione a opção desejada para começarmos</Text>
+            
+            <View style={{position: 'absolute'}}>
+                <Modal
+                transparent={true}
+                visible = {visible}
+                >
+                    <View style={{flex:0.4,backgroundColor: 'pink', alignItems: 'center', justifyContent: 'center', marginHorizontal: 30, marginTop: 10,borderRadius: 20, padding: 20}}>
+                        <Text style={{textAlign: 'center', marginVertical: 10, fontSize: 16}}>Atenção!{"\n"}{"\n"}
+                        Você profissional de saúde também precisa se cuidar!{"\n"}{"\n"}
+                        Fique atento as recomendações quanto a presença de sinais e sintomas, fatores de risco e periodicidade dos exames para rastreamento e detecção precoce do câncer de mama.{"\n"}{"\n"}
+                        Cuide-se!
+                        </Text>
+                        <NewButton textoPrincipal="Fechar"
+                        navigation = {() => {setVisible(false), setOpacity(1)}} />
+                    </View>
+
+                </Modal>
             </View>
-            <View style={styles.button}>
-                <NewButton textoPrincipal="Calculadora de risco (Gail)" navigation = {() => navigation.navigate("Calculator")}/>
+            <View style={{opacity: opacity}}>
+                <View style={styles.button }>
+                    <Text style={styles.text}>Muito bem! Agora selecione a opção desejada para começarmos</Text>
+                </View>
+                <View style={styles.button }>
+                    <NewButton textoPrincipal="Calculadora de risco (Gail)" navigation = {() => navigation.navigate("Calculator")}/>
+                </View>
+                <View style={styles.button }>
+                    <NewButton textoPrincipal="Acesso à informação" navigation = {() => navigation.navigate("Information")}/> 
+                </View>
+                <View style={styles.button }>
+                    <NewButton textoPrincipal="Sobre o aplicativo" navigation = {() => navigation.navigate("Sobre")}/>
+                </View> 
             </View>
-            <View style={styles.button}>
-                <NewButton textoPrincipal="Acesso à informação" navigation = {() => navigation.navigate("Information")}/> 
-            </View>
-            <View style={styles.button}>
-                <NewButton textoPrincipal="Sobre o aplicativo" navigation = {() => navigation.navigate("Sobre")}/>
-            </View> 
+            
         </View>
     )
 }
